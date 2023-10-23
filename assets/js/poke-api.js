69b9fb13b8a03d8b8a02d0ac3fad9952251d140f
vncsbrnd <131720172+vncsbrnd@users.noreply.github.com>
2023-10-22 21:14:00 -0300

    Update poke-api.js

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,35 +1,39 @@
-
-const pokeApi = {}
-
-function convertPokeApiDetailToPokemon(pokeDetail) {
-    const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.id
-    pokemon.name = pokeDetail.name
-
-    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
-    const [type] = types
-
-    pokemon.types = types
-    pokemon.type = type
-
-    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
-
-    return pokemon
-}
-
-pokeApi.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url)
-        .then((response) => response.json())
-        .then(convertPokeApiDetailToPokemon)
-}
-
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
-    return fetch(url)
-        .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-        .then((detailRequests) => Promise.all(detailRequests))
-        .then((pokemonsDetails) => pokemonsDetails)
-}
+const formatPokemonModel = ({ id, name, types, sprites, height, weight, abilities, stats }) => {
+    const pokemon = new PokemonModel();
+    pokemon.order = id;
+    pokemon.name = name.charAt(0).toUpperCase() + name.slice(1);
+    pokemon.types = types.map(({type}) => type.name);
+    pokemon.type = pokemon.types[0];
+    pokemon.img = sprites.other["official-artwork"].front_default;
+    pokemon.height = height;
+    pokemon.weight = weight;
+    pokemon.abilities = abilities.map(({ability}) => ability.name);
+    pokemon.status = stats.reduce((acc, {stat, base_stat}) => {
+      acc[stat.name] = base_stat;
+      return acc;
+    }, {});
+  
+    return pokemon;
+  }
+  
+  const requestPokeDetails = async (url) => {
+    try {
+      const response = await fetch(url);
+      const pokemon = await response.json();
+      return formatPokemonModel(pokemon);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+  
+  const requestPokeApi = async (offset = 0, limit = 10) => {
+    const endpoint = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+    try {
+      const response = await fetch(endpoint);
+      const { results } = await response.json();
+      const pokemonDetails = await Promise.all(results.map(({ url }) => requestPokeDetails(url)));
+      return pokemonDetails;
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
